bf62766501c274c2d16a08060d0d1fac572188c0
SixtenB <sixten.bjorling@student.forsbergsskola.se>
2024-09-23 14:07:39 +0200

    Started extras
    The Coordinate try

---
+++ P12_1Random_Coordinates/Program.cs
@@ -0,0 +1,13 @@
+﻿int x = 100;
+int y = 100;
+
+char empty = 'O';
+
+for (int h = 0; h < y; h++)
+{
+    for (int w = 0; w < x; w++)
+    {
+        Console.Write(empty);
+    }
+    Console.WriteLine();
+}
