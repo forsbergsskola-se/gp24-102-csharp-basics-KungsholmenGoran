6639c2623539c8857e61b7ab0eab26594429cc33
SixtenB <sixten.bjorling@student.forsbergsskola.se>
2024-09-24 09:31:55 +0200

    p12 Random Chance
    Done

---
+++ P12_4Random_Chance/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Channels;
+
+Random random = new Random();
+int chance = random.Next(101);
+
+if (chance <= 10){
+    Console.WriteLine("This is a secret message");
+}
+else{
+    Console.WriteLine("Try again");
+}
