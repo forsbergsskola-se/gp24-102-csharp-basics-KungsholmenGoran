b2ea4eee1969e9694a75e7398f5e7e98bf644e6b
SixtenB <sixten.bjorling@student.forsbergsskola.se>
2024-09-24 09:27:00 +0200

    RandomP12_1 Complete
    .

--- P12_1Random_Coordinates/Program.cs
+++ P12_1Random_Coordinates/Program.cs
@@ -1,13 +1,27 @@
 ﻿int x = 100;
 int y = 100;
-
+int[]row = new int [x];
+int[]column = new int [y];
+Random random = new Random();
 char empty = 'O';
+char enemy = 'X';
+int enemyPosX = random.Next(101);
+int enemyPosY = random.Next(101);
 
 for (int h = 0; h < y; h++)
 {
+    column[h] = h;
     for (int w = 0; w < x; w++)
     {
-        Console.Write(empty);
+        if (h == enemyPosY && w == enemyPosX){
+            Console.Write(enemy);
+        }
+        else{
+            row[w] = w;
+            Console.Write(empty);
+        }
+        
     }
     Console.WriteLine();
 }
+Console.WriteLine($"Enemy spawned at:\nRow: {enemyPosY}\nColumn: {enemyPosX}");
