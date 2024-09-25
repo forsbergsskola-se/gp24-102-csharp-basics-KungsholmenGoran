c816c8cb2a6042457163e85a34787ff0788914f5
SixtenB <sixten.bjorling@student.forsbergsskola.se>
2024-09-25 10:35:05 +0200

    p13-3
    .

---
+++ P13_3Pattern/Program.cs
@@ -0,0 +1,26 @@
+﻿// not sure if I am to use count <= input
+
+Console.WriteLine("Enter a number: ");
+int input = int.Parse(Console.ReadLine());
+int count = 0;
+
+label1:
+if (count < input)
+{
+    Console.WriteLine();
+    for (int i = 0; i < input; i++)
+    {
+        Console.Write("#-");
+    }
+    count++;
+}
+if (count < input)
+{
+    Console.WriteLine();
+    for (int o = 0; o < input; o++)
+    {
+        Console.Write("-#");
+    }
+    count++;
+    goto label1;
+}
